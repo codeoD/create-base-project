efc08b9374dbfe999c4b3cddb1ed54f263e84f0f
codeoD <760206335@qq.com>
2022-03-15 22:46:04 +0800

    init commit

---
+++ template-vue-ts/src/components/HelloWord.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>welcome to use base-project scaffold</div>
+</template>
+<script lang="tsx">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+});
+</script>
+<style lang="postcss"></style>
